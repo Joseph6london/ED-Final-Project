9b739ef376b29be18fcb9189f54c22a3663dceef
Joshua Joseph <161672980+Joseph6london@users.noreply.github.com>
2024-06-18 17:57:25 -0400

    Update Node.h
    Made minor formatting improvements.
    included "Student.h"
    and added using namespace std;

--- Node.h
+++ Node.h
@@ -1,61 +1,65 @@
 #ifndef NODE_H
 #define NODE_H
+#include "Student.h"
+using namespace std;
 
-class Node {
+
+class Node{
 protected:
-   Student* data;
-   Node* next;
-   Node* previous;
+    Student* data;
+    Node* next;
+    Node* previous;
 
 public:
-   // Constructs this node with the specified numerical data value. The next
-   // and previous pointers are each assigned nullptr.
-   Node(Student* student) {
-      data = student;
-      next = nullptr;
-      previous = nullptr;
-   }
+    // Constructs this node with the specified numerical data value. The next
+    // and previous pointers are each assigned nullptr.
+    Node(Student* student) {
+        data = student;
+        next = nullptr;
+        previous = nullptr;
+    }
 
-   // Constructs this node with the specified numerical data value, next
-   // pointer, and previous pointer.
-   Node(Student* student, Node* nextNode, Node* previousNode) {
-      data = student;
-      next = nextNode;
-      previous = previousNode;
-   }
+    // Constructs this node with the specified numerical data value, next
+    // pointer, and previous pointer.
+    Node(Student* student, Node* nextNode, Node* previousNode) {
+        data = student;
+        next = nextNode;
+        previous = previousNode;
+    }
 
-   virtual ~Node() {
-   }
+    virtual ~Node() {
+    }
 
-   // Returns this node's data.
-   virtual double GetData() {
-      return data;
-   }
+    // Returns this node's data.
+    virtual Student* GetData() {
+        return data;
+    }
 
-   // Sets this node's data.
-   virtual void SetData(Student* newStudent) {
-      data = newStudent;
-   }
+    // Sets this node's data.
+    virtual void SetData(Student* newStudent) {
+        data = newStudent;
+    }
 
-   // Gets this node's next pointer.
-   virtual Node* GetNext() {
-      return next;
-   }
+    // Gets this node's next pointer.
+    virtual Node* GetNext() {
+        return next;
+    }
 
-   // Sets this node's next pointer.
-   virtual void SetNext(Node* newNext) {
-      next = newNext;
-   }
+    // Sets this node's next pointer.
+    virtual void SetNext(Node* newNext) {
+        next = newNext;
+    }
 
-   // Gets this node's previous pointer.
-   virtual Node* GetPrevious() {
-      return previous;
-   }
+    // Gets this node's previous pointer.
+    virtual Node* GetPrevious() {
+        return previous;
+    }
 
-   // Sets this node's previous pointer.
-   virtual void SetPrevious(Node* newPrevious) {
-      previous = newPrevious;
-   }
+    // Sets this node's previous pointer.
+    virtual void SetPrevious(Node* newPrevious) {
+        previous = newPrevious;
+    }
+
 };
 
-#endif
+#endif 
