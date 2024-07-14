54d6f97144aee4ff3c9bf50d3d082009eb6cc7dc
Joshua Joseph <161672980+Joseph6london@users.noreply.github.com>
2024-07-14 15:36:20 -0400

    Update main.cpp
    Progress on test functions/menu.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include "Student.h"
 #include "Node.h"
-#include "LinkedList.h"
-
+#include "sortedlinkedlist.h"
+#include "test_program.h"
+#include "text_to_askii_dictionary.h"
+#include <limits>
 using namespace std;
 
-int main() {
-    int call;
+int main()
+{
+    string FirstName,LastName, Mnumber, Birthday;
+    double GPA = 0;
+    int call, Index;
+    bool GPAmaybe;
     cout<<"Linked list test program"<<endl;
-    cout<<"1: Constructor"<<endl;
+    //cout<<"1: Constructor"<<endl;
     cout<<"2: Add item"<<endl;
     cout<<"3: Get item"<<endl;
     cout<<"4: Is in list?"<<endl;
@@ -19,43 +26,47 @@ int main() {
     cout<<"7: See next"<<endl;
     cout<<"8: See at"<<endl;
     cout<<"9: Reset"<<endl;
-    cout<<"10: Destructor"<<endl;
+    //cout<<"10: Destructor"<<endl;
     cout<<"11: End run"<<endl;
     cout<<""<<endl;
     cout<<"Enter a function number (1-10)"<<endl;
-    cin>>call;
-
+    cin>> call;
+    cout << call << endl;
+    SortedLinkedList list;
     if (call==1) {
-        cout<<""<<endl;
-        cin>>;
+        cout<<"Input First Name"<<endl;
+        cin>> FirstName;
+        cout<<"Do you want to input a gpa "<<endl;
+        cin>> GPAmaybe;
+        Student* testStudent = new Student(FirstName, LastName, Mnumber, Birthday, GPA);
+        list.addItem(testStudent);
     } else if (call==2) {
-        cout<<""<<endl;
-        cin>>;
+        cout<<"whats the Mnumber of the student you wish to get (Include the M)"<<endl;
+        cin>> Mnumber;
+        list.getItem(Mnumber);
     } else if (call==3) {
-        cout<<""<<endl;
-        cin>>;
+        cout<<"whats the Mnumber of the student you wish to find (Include the M)"<<endl;
+        cin>> Mnumber;
+        list.isInList(Mnumber);
     } else if (call==4) {
         cout<<""<<endl;
-        cin>>;
+        list.isEmpty();
     } else if (call==5) {
         cout<<""<<endl;
-        cin>>;
+        list.size();
     } else if (call==6) {
         cout<<""<<endl;
-        cin>>;
+        list.seeNext();
     } else if (call==7) {
-        cout<<""<<endl;
-        cin>>;
+        cout<<"Whats the index of the student?: "<<endl;
+        cin>> Index;
+        list.seeAt(Index);
     } else if (call==8) {
-        cout<<""<<endl;
-        cin>>;
+        list.reset();
     } else if (call==9) {
         cout<<""<<endl;
         cin>>;
     } else if (call==10) {
-        cout<<""<<endl;
-        cin>>;
-    } else if (call==11) {
         cout<<"Ending program"<<endl;
         return 0;
     } else {
