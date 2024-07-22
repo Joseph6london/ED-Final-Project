70d4c70403ccddbb21ac32042f39915ba6d7309a
JasperGains <157747378+JasperGains@users.noreply.github.com>
2024-07-22 12:24:16 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,65 +12,75 @@ using namespace std;
 
 int main()
 {
-    string FirstName,LastName, Mnumber, Birthday;
-    double GPA = 0;
-    int call, Index;
-    bool GPAmaybe;
-    cout<<"Linked list test program"<<endl;
-    //cout<<"1: Constructor"<<endl;
-    cout<<"2: Add item"<<endl;
-    cout<<"3: Get item"<<endl;
-    cout<<"4: Is in list?"<<endl;
-    cout<<"5: Is empty?"<<endl;
-    cout<<"6: Size"<<endl;
-    cout<<"7: See next"<<endl;
-    cout<<"8: See at"<<endl;
-    cout<<"9: Reset"<<endl;
-    //cout<<"10: Destructor"<<endl;
-    cout<<"11: End run"<<endl;
-    cout<<""<<endl;
-    cout<<"Enter a function number (1-10)"<<endl;
-    cin>> call;
-    cout << call << endl;
-    SortedLinkedList list;
-    if (call==1) {
-        cout<<"Input First Name"<<endl;
-        cin>> FirstName;
-        cout<<"Do you want to input a gpa "<<endl;
-        cin>> GPAmaybe;
-        Student* testStudent = new Student(FirstName, LastName, Mnumber, Birthday, GPA);
-        list.addItem(testStudent);
-    } else if (call==2) {
-        cout<<"whats the Mnumber of the student you wish to get (Include the M)"<<endl;
-        cin>> Mnumber;
-        list.getItem(Mnumber);
-    } else if (call==3) {
-        cout<<"whats the Mnumber of the student you wish to find (Include the M)"<<endl;
-        cin>> Mnumber;
-        list.isInList(Mnumber);
-    } else if (call==4) {
+    while(true) {
+        string FirstName,LastName, Mnumber, Birthday;
+        double GPA = 0;
+        int call, Index;
+        bool GPAmaybe;
+        cout<<"Linked list test program"<<endl;
+        //cout<<"1: Constructor"<<endl;
+        cout<<"1: Add item"<<endl;
+        cout<<"2: Get item"<<endl;
+        cout<<"3: Is in list?"<<endl;
+        cout<<"4: Is empty?"<<endl;
+        cout<<"5: Size"<<endl;
+        cout<<"6: See next"<<endl;
+        cout<<"7: See at"<<endl;
+        cout<<"8: Reset"<<endl;
+        cout<<"9: ASCII List"<<endl;
+        cout<<"10: End run"<<endl;
         cout<<""<<endl;
-        list.isEmpty();
-    } else if (call==5) {
-        cout<<""<<endl;
-        list.size();
-    } else if (call==6) {
-        cout<<""<<endl;
-        list.seeNext();
-    } else if (call==7) {
-        cout<<"Whats the index of the student?: "<<endl;
-        cin>> Index;
-        list.seeAt(Index);
-    } else if (call==8) {
-        list.reset();
-    } else if (call==9) {
-        cout<<""<<endl;
-        cin>>;
-    } else if (call==10) {
-        cout<<"Ending program"<<endl;
-        return 0;
-    } else {
-        cout<<"Error: invalid function number"<<endl;
+        cout<<"Enter a function number (1-10)"<<endl;
+        cin>> call;
+        cout << call << endl;
+        SortedLinkedList list;
+        if (call==1) {
+            cout<<"Input First Name"<<endl;
+            cin>> FirstName;
+            cout<<"Input Last Name"<<endl;
+            cin>> LastName;
+            cout<<"Input Birthday"<<endl;
+            cin>> Birthday;
+            cout<<"Do you want to input a gpa "<<endl;
+            cin>> GPAmaybe;
+            if (GPAmaybe) {
+                cout<<"Input GPA"<<endl;
+                cin>> GPA;
+            }
+            Student* testStudent = new Student(FirstName, LastName, Mnumber, Birthday, GPA);
+            list.addItem(testStudent);
+        } else if (call==2) {
+            cout<<"whats the Mnumber of the student you wish to get (Include the M)"<<endl;
+            cin>> Mnumber;
+            list.getItem(Mnumber);
+        } else if (call==3) {
+            cout<<"whats the Mnumber of the student you wish to find (Include the M)"<<endl;
+            cin>> Mnumber;
+            list.isInList(Mnumber);
+        } else if (call==4) {
+            cout<<""<<endl;
+            list.isEmpty();
+        } else if (call==5) {
+            cout<<""<<endl;
+            list.size();
+        } else if (call==6) {
+            cout<<""<<endl;
+            list.seeNext();
+        } else if (call==7) {
+            cout<<"Whats the index of the student?: "<<endl;
+            cin>> Index;
+            list.seeAt(Index);
+        } else if (call==8) {
+            list.reset();
+        } else if (call==9) {
+            cout<<""<<endl;
+            cin>>;
+        } else if (call==10) {
+            cout<<"Ending program"<<endl;
+            return 0;
+        } else {
+            cout<<"Error: invalid function number"<<endl;
+        } 
     }
     return 0;
 }
